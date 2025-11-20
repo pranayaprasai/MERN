21f22be57758606d859a5f1d9b448fe60c98d404
pranayaprasai <pranayaprasai01@gmail.com>
2025-11-20 09:46:51 +0545

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Box,useColorModeValue } from "@chakra-ui/react";
 import { Route,Routes } from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import Navbar from "./components/Navbar";
 
